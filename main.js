aac25d4278b868ca2c63ff6d789fc1e546b808b3
krishjain-2301 <jaink@gmail.com>
2026-05-30 23:36:37 +0530

    Obfuscate allowed names by hashing

--- main.js
+++ main.js
@@ -109,9 +109,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const hashedName = await hashString(rawName);
         
-        const allowedNames = ['arya', 'anirudh', 'harsha', 'ritvik', 'atri', 'karman', 'karan', 'arshit', 'ayush', 'vikram', 'gracy', 'kumudwini', 'jasmitha', 'advait', 'dhanunjay', 'snehitha'];
+        const allowedHashes = [
+            'b02e06485c54bb8d503d44ab75693665ccfb580a0d5cbd12c488d8c4aab51f37', // arya
+            '77567c4f0cd9d99cc9aa598b2bb7f7532d5da3b242d360f866985e7b3ee20511', // anirudh
+            '082f51aba47229235d342eb95be14ddf7b2acb03d9d27c4c22225c2ff8fd11f4', // harsha
+            'a961998faf6865df361c0802b622f76aefa9cc93b8ccbb86ace88b1676216c8e', // ritvik
+            'f66112448c73c57005461a10e1f975b65a37903c31d6b68661926875863c0a13', // atri
+            'c5fa7b4dcf07033c0287feafb6730f7625f420653e880327dd2447b8ba839583', // karman
+            '46ed260db5a4cb33871f0b308aae3e899602cd7f20c6841677e4079d8b9e5ec3', // karan
+            '9ec511ea45cb4b20c5086edb481c034fd0e2c72d9bcbb8b9c372374c180092cc', // arshit
+            '3848e9eff8d31953e3762251e377bb08f78099d2f2193740cd0523738da8b1a5', // ayush
+            '4b7bb8c301065be38ebed64ed1e85e4ef6f6a06210b6c29abc5919619819b7c0', // vikram
+            '62aeea5d7c6251b200e685d78d69031fc39cf1061a4c8fd1c8c0746a52b09565', // gracy
+            'c7b3fec43d69b5a0e648f534eff656c1fd7e507fc730af47336a0d3587cb77eb', // kumudwini
+            '682aadef54148f6c969fce014cca450975b72f982bf7e1f3afff1af82cae0b13', // jasmitha
+            'abdd3ff29b9324bb375bf8e195349ac779693fa03a855b2788f30754ff77dee8', // advait
+            '759797a57c1a4deeeb9f688914029794c85677dd53379f2023bf74d5796c64f5', // dhanunjay
+            'c6b954680120ef4bcca51c7f307576bba6d20cf4272751536405d66723df70f6', // snehitha
+            'b30a0e34683cf2594ffa4b9f9b2379d314ff57d48b9673a93ea2a29361805ef0', // jatin
+            '2825a3abf211b3c12c5e1e08226528e27d0f819c394e65f3106d6a017d0e89b1', // hardik
+            '1795aba0bc169c6eeabf09e4a6d5bf09e91bfcbb59bc36282fe2d3ba3b45f46b', // vinayak
+            '23eb127b3caf920d3dd72f834b35b73e5b8bda870b0d7147838d182b5a9dd789'  // alvin
+        ];
         
-        if (!allowedNames.includes(rawName)) {
+        if (!allowedHashes.includes(hashedName)) {
             nameInput.classList.add('error-flash');
             setTimeout(() => nameInput.classList.remove('error-flash'), 500);
             nameInput.value = "";
